9dbc7f322f2c27df3509fd2919018a362600f1ad
Imanullah <pengguna21@gmail.com>
2023-10-21 13:43:46 +0700

    fix | dashboard page

--- src/pages/backoffice/dashboard.tsx
+++ src/pages/backoffice/dashboard.tsx
@@ -1,13 +1,10 @@
 import BackofficeLayout from '@/layouts/BackofficeLayout';
 import { Typography } from 'antd';
-import { useSession } from 'next-auth/react';
 
 export default function Dashboard() {
-  const session = useSession();
-
   return (
-    <BackofficeLayout title="Dashboard" breadcrumbs={[{ title: 'Dashboard' }]}>
-      <Typography.Text>{JSON.stringify(session.data?.user)}</Typography.Text>
+    <BackofficeLayout title="Dashboard">
+      <Typography.Text></Typography.Text>
     </BackofficeLayout>
   );
 }
